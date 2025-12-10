369f8bc2d0c9612ff0d73c74c039ed54b784af61
Maksim Razduyeu <mrazduev@gmail.com>
2025-12-11 00:54:19 +0300

    fix: Clean Pydantic models

--- lecture_5/book_api/schemas.py
+++ lecture_5/book_api/schemas.py
@@ -8,27 +8,18 @@ class BookRequest(BaseModel):
     title: str = Field(
         min_length=1,
         max_length=255,
-        json_schema_extra={
-            "title": "Book Title",
-            "description": "The title of the book.",
-            "example": "Title",
-        },
+        description="The title of the book.",
+        example="Title"
     )
     author: str = Field(
         min_length=1,
         max_length=255,
-        json_schema_extra={
-            "title": "Book Author",
-            "description": "The author of the book.",
-            "example": "Author",
-        },
+        description="The author of the book.",
+        example="Author"
     )
     year: Optional[int] = Field(
-        json_schema_extra={
-            "title": "Publication Year",
-            "description": "Publication year.",
-            "example": 0,
-        },
+        description="Publication year.",
+        example=0
     )
 
 
@@ -38,23 +29,24 @@ class BookRequest(BaseModel):
 class BookResponse(BaseModel):
     id: int = Field(
         description="Unique identifier of the book.",
-        json_schema_extra={"example": 1},
+        example=1
     )
     title: str = Field(
         min_length=1,
         max_length=255,
         description="The title of the book.",
-        json_schema_extra={"example": "Title"},
+        example="Title"
     )
     author: str = Field(
         min_length=1,
         max_length=255,
         description="The author of the book.",
-        json_schema_extra={"example": "Author"},
+        example="Author"
     )
     year: Optional[int] = Field(
         description="Publication year.",
-        json_schema_extra={"example": 0},
+        example=0
     )
 
-    model_config = {"from_attributes": True}
+    class Config:
+        from_attributes = True
